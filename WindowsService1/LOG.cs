68484520696635ca7026c46e70ccd59c39c1685d
Mohammedashrafhabib <93453956+Mohammedashrafhabib@users.noreply.github.com>
2022-07-24 15:30:06 +0200

    Add project files.

---
+++ WindowsService1/LOG.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace WindowsService1
+{
+    internal class LOG
+    {
+        internal static void write(string v)
+        {
+            StreamWriter sw = new StreamWriter(@"C:\Users\mando\OneDrive\Desktop\service.txt", true);
+            sw.WriteLine(v);
+            sw.Close();
+        }
+    }
+}
---
+++ WindowsService1/Service1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.IO;
+namespace WindowsService1
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+        Timer timer;
+        
+        protected override void OnStart(string[] args)
+        {
+            LOG.write("Service is start at " + DateTime.Now);
+            timer = new Timer();
+            timer.Start();
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Interval = 5000;
+            timer.Enabled = true;
+        }
+        private void OnElapsedTime(object source, ElapsedEventArgs e)
+        {
+            LOG.write("Service is running at " + DateTime.Now);
+        }
+        protected override void OnStop()
+        {
+            LOG.write("Service is stoped at " + DateTime.Now);
+        }
+    }
+}
